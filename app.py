e6099d65977a8b3f49f04ef9993afd283cbe664e
Liu Jinyao <liujinyao@pjlab.com>
2023-07-06 18:42:50 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from typing import List
+
+import uvicorn
+from fastapi import FastAPI, UploadFile, Response
+
+from inference import inferencer
+
+app = FastAPI()
+
+@app.post("/")
+async def root(files: UploadFile, response: Response):
+    classification_result = inferencer(Image.open(files.file))
+    if not isinstance(classification_result, List):
+        response.status_code = 400
+        return "result type must be a list"
+
+    return classification_result
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
--- inference.py
+++ inference.py
@@ -8,9 +8,16 @@ model_name = "resnet50_8xb32_in1k"
 
 image_inferencer = ImageClassificationInferencer(model_name)
 
+def post_process(out):
+    out['pred_scores'] = out['pred_scores'].tolist()
+    return out
+
 class Inferencer():
 
     def __call__(self, image: Image.Image):
-        return image_inferencer(np.asarray(image))
+        outputs = image_inferencer(np.asarray(image))
+        
+        return [ post_process(out) for out in outputs]
+
 
 inferencer = Inferencer()
