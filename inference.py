7e8906ae6baac18928ba136e24cf5aaf51d8a5be
Liu Jinyao <liujinyao@pjlab.com>
2023-07-06 13:59:39 +0800

    fix inference.py

--- inference.py
+++ inference.py
@@ -1,23 +1,16 @@
-import uuid
+
 from PIL import Image
 
-from mmagic.apis import MMagicInferencer
+import numpy as np
+from mmpretrain.apis import ImageClassificationInferencer
 
-# reference: https://github.com/open-mmlab/mmagic/tree/main/configs/controlnet
+model_name = "resnet50_8xb32_in1kn"
 
-model_name = "controlnet"
-device_id = "cuda:0"
-
-# controlnet-canny
-# 如果没有设置device, 优先使用gpu,其次使用cpu
-agic_inferencer = MMagicInferencer(model_name, model_setting=1)
+image_inferencer = ImageClassificationInferencer(model_name)
 
 class Inferencer():
 
-    def __call__(self, text_prompts, image_control: Image.Image) -> Image.Image:
-        image_path = str(uuid.uuid1()) + ".jpg"
-        image_control.save(image_path)
-        outputs = agic_inferencer.infer(text=text_prompts, control=image_path)
-        return outputs[1][0]
+    def __call__(self, image: Image.Image):
+        return image_inferencer(np.asarray(image))
 
 inferencer = Inferencer()
