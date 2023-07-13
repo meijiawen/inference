f8fba107ccc6c90d330d15af56a1d8868014ee4f
meijiawen <96159704+meijiawen@users.noreply.github.com>
2023-07-13 10:21:57 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,33 +1,29 @@
-import torch
-# from .base import CustomInferencer
-# from .utils import ClassificationResult, ImageType
-from transformers import AutoImageProcessor, ResNetForImageClassification
-from datasets import load_dataset
+from transformers import BeitFeatureExtractor, BeitForImageClassification
+from PIL import Image
+import requests
 
-model_name = "microsoft/resnet-50"
-
-# processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
-# model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+feature_extractor = BeitFeatureExtractor.from_pretrained(
+    'microsoft/beit-base-patch16-384')
+model = BeitForImageClassification.from_pretrained(
+    'microsoft/beit-base-patch16-384')
 
 
 class Inferencer():
 
-    def __init__(self):
-        self.processor = AutoImageProcessor.from_pretrained(
-            "microsoft/resnet-50")
-        self.model = ResNetForImageClassification.from_pretrained(
-            "microsoft/resnet-50")
+    # def __init__(self):
+    #     self.feature_extractor = BeitFeatureExtractor.from_pretrained(
+    #         'microsoft/beit-base-patch16-384')
+    #     self.model = BeitForImageClassification.from_pretrained(
+    #         'microsoft/beit-base-patch16-384')
 
-    def __call__(self, img):
-        inputs = self.processor(img, return_tensors="pt")
-        with torch.no_grad():
-            logits = self.model(**inputs).logits
-        predicted_label = logits.argmax(-1).item()
-        return self.model.config.id2label[predicted_label]
+    def __call__(self, imageUrl):
+        image = Image.open(requests.get(imageUrl, stream=True).raw)
+        inputs = feature_extractor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        # model predicts one of the 1000 ImageNet classes
+        predicted_class_idx = logits.argmax(-1).item()
+        print("Predicted class:", model.config.id2label[predicted_class_idx])
+
 
 inferencer = Inferencer()
-# dataset = load_dataset("huggingface/cats-image")
-# image = dataset["test"]["image"][0]
-# inferencer = ImageClassification()
-# output = inferencer(img=image)
-# print(output)
