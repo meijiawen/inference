0e7a7152a3999d6c7e1327a9fa48ef37ec944b26
meijiawen <meijiawen@pjlab.org.cn>
2023-07-13 19:24:19 +0800

    timm

--- inference.py
+++ inference.py
@@ -3,25 +3,26 @@ from PIL import Image
 import timm
 import torch
 
-
-def post_process(out):
-    out['pred_scores'] = out['pred_scores'].tolist()
-    return out
+# def post_process(out):
+#     out['pred_scores'] = out['pred_scores'].tolist()
+#     return out
 
 
 class Inferencer():
 
-    def __call__(self, image: Image.Image):
-        model = timm.create_model(
+    def __init__(self) -> None:
+        self.model = timm.create_model(
             'vit_large_patch14_clip_224.openai_ft_in12k_in1k', pretrained=True)
-        model = model.eval()
+        self.model = self.model.eval()
 
         # get model specific transforms (normalization, resize)
-        data_config = timm.data.resolve_model_data_config(model)
-        transforms = timm.data.create_transform(**data_config,
-                                                is_training=False)
+        self.data_config = timm.data.resolve_model_data_config(self.model)
+        self.transforms = timm.data.create_transform(**self.data_config,
+                                                     is_training=False)
 
-        output = model(transforms(image).unsqueeze(
+    def __call__(self, image: Image.Image):
+
+        output = self.model(self.transforms(image).unsqueeze(
             0))  # unsqueeze single image into batch of 1
 
         top5_probabilities, top5_class_indices = torch.topk(
