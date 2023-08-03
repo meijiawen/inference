6f606d5e9cb4801a852a42c89b483c28a6f3146a
meijiawen <96159704+meijiawen@users.noreply.github.com>
2023-08-03 13:57:13 +0800

    Update inferencer.py

--- inferencer.py
+++ inferencer.py
@@ -12,6 +12,9 @@ class Inferencer():
 
     def __call__(self, image: Image.Image,**kwargs) -> Image.Image:
         out = det_inferencer(np.array(image))
+        print("first normal arg:", image)
+        for key, value in kwargs.items():
+            print("{0} = {1}".format(key, value))
         return out
 
 inferencer = Inferencer()
