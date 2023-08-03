4a7d28ba3797f0ae1fa4d6e33635fd8d5d0fcbc9
meijiawen <96159704+meijiawen@users.noreply.github.com>
2023-08-03 10:23:54 +0800

    Update inferencer.py

--- inferencer.py
+++ inferencer.py
@@ -1,10 +1,17 @@
 
 from PIL import Image
 
+import numpy as np
+from mmdet.apis import DetInferencer
+
+
+det_inferencer = DetInferencer("faster-rcnn_r50-caffe-c4_1x_coco")
+
 
 class Inferencer():
 
     def __call__(self, image: Image.Image) -> Image.Image:
-        return image
+        out = det_inferencer(np.array(image))
+        return out
 
 inferencer = Inferencer()
