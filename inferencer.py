6b2cafb8f2477f617588ecf05e7bac75c970e5df
meijiawen <96159704+meijiawen@users.noreply.github.com>
2023-08-03 16:22:24 +0800

    Update inferencer.py

--- inferencer.py
+++ inferencer.py
@@ -5,6 +5,9 @@ from PIL import Image
 class Inferencer():
 
     def __call__(self, image: Image.Image) -> Image.Image:
-        return image
+        processed_images = []
+        for image in images:
+            processed_images.append(image)  
+        return processed_images
 
 inferencer = Inferencer()
